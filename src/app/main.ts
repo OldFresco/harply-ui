13ee8219ecc2a950cbcca6208dddc930b6b0022c
OldFresco <hinsonkojo@gmail.com>
2016-08-08 11:43:24 +0100

    Enable prod mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 import { ConvoService } from './services/convo.service';
+import {enableProdMode} from '@angular/core';
 
-
+enableProdMode();
 bootstrap(AppComponent, [ConvoService]);

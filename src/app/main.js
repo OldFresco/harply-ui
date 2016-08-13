c1e58f2b83ef0d8ce70a0d5d90a015fd07528b6c
OldFresco <hinsonkojo@gmail.com>
2016-08-13 12:15:09 +0100

    Add hash location strategy for azure deployment

--- src/app/main.js
+++ src/app/main.js
@@ -5,6 +5,7 @@ var convo_service_1 = require('./services/convo.service');
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
 var app_routes_1 = require('./app.routes');
+var common_1 = require('@angular/common');
 core_1.enableProdMode();
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [convo_service_1.ConvoService, app_routes_1.APP_ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS]);
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [convo_service_1.ConvoService, app_routes_1.APP_ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS, { provide: common_1.LocationStrategy, useClass: common_1.HashLocationStrategy }]);
 //# sourceMappingURL=main.js.map
--- src/app/main.ts
+++ src/app/main.ts
@@ -4,6 +4,14 @@ import { ConvoService } from './services/convo.service';
 import { enableProdMode } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
+import {  
+  PlatformLocation,  
+  Location,  
+  LocationStrategy,  
+  HashLocationStrategy,  
+  PathLocationStrategy,  
+  APP_BASE_HREF}  
+from '@angular/common'; 
 
 enableProdMode();
-bootstrap(AppComponent, [ConvoService, APP_ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(AppComponent, [ConvoService, APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, {provide: LocationStrategy, useClass: HashLocationStrategy}]);

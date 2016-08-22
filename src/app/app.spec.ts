0198f103b0071b8608d8f6c2dbb98ca9acbd04d8
Kojo Hinson <hinsonkojo@gmail.com>
2016-08-22 11:26:19 +0100

    Weird typescript referencing issue on windows

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,28 +1,6 @@
-import {
-    async,
-    inject,
-    TestComponentBuilder,
-    addProviders
-} from '@angular/core/testing';
-
-import {
-    provide
-} from '@angular/core';
-
-import { AppComponent } from './app.component';
-
 describe('toBeTruthy', function () {
     it('passes if subject is true', function () {
         expect(true).toBeTruthy();
         expect(false).not.toBeTruthy();
     });
 })
-
-it('should make our app Component',
-    async(inject([TestComponentBuilder], (tcb) => {
-        return tcb
-            .createAsync(AppComponent)
-            .then((fixture) => {
-                fixture.AppName = '';
-            });
-    })));
